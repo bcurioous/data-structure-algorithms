17346b4dbc0df6cfef5f00a0ba4d15ed6e455636
James Christian <jamescrstn@gmail.com>
2022-05-21 00:01:11 +0530

    binary-serach added

---
+++ searching/binary-searching.js
@@ -0,0 +1,44 @@
+function binarySearch(items, key) {
+  let start = 0,
+    end = items.length - 1;
+
+  while (start !== end) {
+    const mid = Math.round((start + end) / 2);
+      console.log(start, " :: ", mid, " :: ", end);
+
+    if (key === items[start]) {
+      return start;
+    }
+    if (key === items[mid]) {
+      return mid;
+    }
+    if (key === items[end]) {
+      return end;
+    }
+
+    if (key > items[mid]) {
+      start = mid;
+    }
+    if (key < items[mid]) {
+      end = mid;
+    }
+  }
+
+  return -1;
+}
+
+let list = [1, 3, 5, 7, 9, 10, 14, 19];
+let key = 14;
+
+let result = binarySearch(list, key);
+
+console.log("result :: ", result);
+
+//   [1,2,3]  , key=1
+
+//   1. start = 0 , end = 2, mid = 0+2/2  = 1,
+//      key > items[mid] = 1 > 2 , no
+//      key < items[mid] = 1 < 2 , yes
+//        end = 1
+
+//   2. start = 0 , end = 1, mid = 0+1/2 = 1
